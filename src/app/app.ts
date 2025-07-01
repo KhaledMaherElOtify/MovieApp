b84a280f0dbe230a4399230b1d2dc6147d4a9fd2
Khaled 20353423 <Khaled.20353423@compit.aun.edu.eg>
2025-07-01 14:01:17 +0300

    add home page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './components/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [RouterOutlet, Home],
+   template: `
+    <router-outlet></router-outlet>
+  `,
 })
+
 export class App {
   protected title = 'MovieApp';
 }
